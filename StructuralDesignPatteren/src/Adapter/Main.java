c10d2177deabb086dca451764b9daf33de221e82
Murali14898 <samalmurali575@gmail.com>
2026-03-20 10:14:02 +0530

    Adapter design pattern

--- StructuralDesignPatteren/src/Adapter/Main.java
+++ StructuralDesignPatteren/src/Adapter/Main.java
@@ -5,6 +5,13 @@ package Adapter;
  * Then that library should plugged by using Adapter design patteren
  * If you follow this patteren, in future if one third party library denies for the services you can
  * easily switch to other third party library
+ * 
+ * Ex2: Taking Zomoto as example
+ * Suppose Zomoto is accepting the money you paid for food Via SBI bank
+ * After few days SBI BANK stops its service providing to Zomato
+ * If zomato didn't follow the Adapter DP then Switching to Different bank causes problem like
+ *  1. Removing all code which was written for SBI bank
+ *  2. Tightly coupled
  */
 
 public class Main {
